74f387ba05f073ba80ad9dd253627b831de38230
Nicholas Nanda <nikolasnanda@gmail.com>
2019-10-23 14:53:31 +0700

    Update koneksi.php

--- CRUD Sederhana/koneksi.php
+++ CRUD Sederhana/koneksi.php
@@ -1,5 +1,5 @@
 <?php  
-$konek = new mysqli ("localhost","root","","materi");
+$konek = new mysqli ("localhost","root","","db_hujikol");
 if ($konek->connect_error) {
 	//jika terjadi error, matikan proses dengan die() atau exit()
 	die('Maaf koneksi gagal: '. $konek->connect_error);
