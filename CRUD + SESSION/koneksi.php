f66540242f9aecf2cb988da9e137f025398655cd
Nicholas Nanda <nikolasnanda@gmail.com>
2019-11-06 14:48:55 +0700

    Delete koneksi.php

--- CRUD + SESSION/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php  
-$konek = new mysqli ("localhost","root","","db_hujikol");
-if ($konek->connect_error) {
-	//jika terjadi error, matikan proses dengan die() atau exit()
-	die('Maaf koneksi gagal: '. $konek->connect_error);
-}
-?>
